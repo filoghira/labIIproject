25ff0816f2c703b6bc8198923439d0b6f26d936d
Filippo Ghirardini <filippo.ghirardini.ra@gmail.com>
2024-09-03 18:19:59 +0200

    Small fixes

--- utils.c
+++ utils.c
@@ -1,15 +1,5 @@
-#include <stdbool.h>
 #include <stddef.h>
 #include "utils.h"
-
-#include <stdio.h>
-
-#define BUF_SIZE 65536
-
-// Funzione che controlla se un arco è valido
-bool valid_arc(const int i, const int j, const int N){
-    return i >= 0 && i < N && j >= 0 && j < N ;
-}
 
 // Funzione che inizializza un array di double con un valore
 void set_array(double *a, const int n, const double v){
@@ -39,44 +29,7 @@ int compare_desc(const void *a, const void *b) {
     return 0;
 }
 
-void print_graph(const grafo *g){
-    fprintf(stderr, "Matrice di adiacenza:\n");
-    for(int i = 0; i < g->N; i++){
-        fprintf(stderr, "Node %d: ", i);
-        for(int j = 0; j < g->in->size[i]; j++){
-            fprintf(stderr,"%d ", g->in->list[i][j]);
-        }
-        fprintf(stderr, "\n");
-    }
-
-    fprintf(stderr, "Nodi uscenti:\n");
-    for(int i = 0; i < g->N; i++){
-        fprintf(stderr, "Node %d: %d\n", i, g->out[i]);
-    }
-}
-
 int custom_compare(const void *a, const void *b)
 {
     return (*(int *)a - *(int *)b);
-}
-
-int count_lines(FILE* file)
-{
-    int counter = 0;
-    for(;;)
-    {
-        char buf[BUF_SIZE];
-        const size_t res = fread(buf, 1, BUF_SIZE, file);
-        if (ferror(file))
-            return -1;
-
-        for(int i = 0; i < res; i++)
-            if (buf[i] == '\n')
-                counter++;
-
-        if (feof(file))
-            break;
-    }
-
-    return counter;
 }
--- utils.h
+++ utils.h
@@ -6,12 +6,9 @@
 
 #include "structures.h"
 
-bool valid_arc(int i, int j, int N);
 void set_array(double *a, int n, double v);
 int dead_end(const grafo *g);
 int compare_desc(const void *a, const void *b);
-void print_graph(const grafo *g);
 int custom_compare(const void *a, const void *b);
-int count_lines(FILE* file);
 
 #endif
